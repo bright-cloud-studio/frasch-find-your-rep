fcb9590077b4360ce5b440b7ee9544819da063f2
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-04-10 13:12:44 -0400

    Update ModFindYourRep.php

--- src/Resources/contao/library/Bcs/Module/ModFindYourRep.php
+++ src/Resources/contao/library/Bcs/Module/ModFindYourRep.php
@@ -1,16 +1,5 @@
 <?php
 
-/**
- * Bright Cloud Studio's Find Your Rep
- *
- * Copyright (C) 2023 Bright Cloud Studio
- *
- * @package    bright-cloud-studio/frasch-find-your-rep
- * @link       https://www.brightcloudstudio.com/
- * @license    http://opensource.org/licenses/lgpl-3.0.html
- */
-
-  
 namespace Bcs\Module;
 
 use Contao\BackendTemplate;
