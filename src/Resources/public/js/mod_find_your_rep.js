670fc517010af50ba26f4527c18d7b7d082d4652
johnadapt <2372574+johnadapt@users.noreply.github.com>
2026-06-29 15:23:06 -0700

    Update mod_find_your_rep.js

--- src/Resources/public/js/mod_find_your_rep.js
+++ src/Resources/public/js/mod_find_your_rep.js
@@ -1,38 +1,65 @@
 
 // When the page is loaded
 $( document ).ready(function() {
-    
-    var entry_found = false;
-    
-    // When our 'select_your_state' select changes
-    $( ".select_your_state" ).on( "change", function() {
 
-        // store our selected state
-        var selectedState = this.value;
-        
-        // Show our state header
-        $('#state_heading').html($(this).find('option:selected').text());
+    var $stateSelect       = $( ".select_your_state" );
+    var $productLineWrap    = $( ".select_product_line" );
+    var $productLineSelect  = $( ".product_line" );
 
-        
-        // Loop through each listing
+    // Run the combined state + product line filter
+    function applyFilter() {
+
+        var selectedState        = $stateSelect.val();
+        var selectedProductLine  = $productLineSelect.val();
+
+        // Nothing chosen yet: hide everything and bail
+        if ( ! selectedState ) {
+            $( ".rep_list .rep" ).hide();
+            $( '#empty' ).hide();
+            return;
+        }
+
+        var entryFound = false;
+
         $( ".rep_list .rep" ).each(function() {
-            
-            // If this rep has our selected state in it's class list, show the rep
-            if($(this).hasClass(selectedState)) {
-                
-                $(this).show();
-                entry_found = true;
-                
+
+            var $rep = $( this );
+
+            // Must match the selected state
+            var matchesState = $rep.hasClass( selectedState );
+
+            // Match product line only when one is selected.
+            // data-product-line holds a space-separated list, e.g. "full_line corporate"
+            var matchesProductLine = true;
+            if ( selectedProductLine ) {
+                var lines = ( $rep.attr( 'data-product-line' ) || '' ).split( /\s+/ );
+                matchesProductLine = ( $.inArray( selectedProductLine, lines ) !== -1 );
+            }
+
+            if ( matchesState && matchesProductLine ) {
+                $rep.show();
+                entryFound = true;
             } else {
-                $(this).hide();
+                $rep.hide();
             }
         });
-        
-        if(!entry_found)
-            $('#empty').show();
-        else
-            $('#empty').hide();
-        
+
+        $( '#empty' ).toggle( ! entryFound );
+    }
+
+    // When a state is selected
+    $stateSelect.on( "change", function() {
+
+        // Show our state header
+        $( '#state_heading' ).html( $( this ).find( 'option:selected' ).text() );
+
+        // Reveal the product line dropdown now that a state is chosen
+        $productLineWrap.show();
+
+        applyFilter();
     });
-    
+
+    // When a product line is selected
+    $productLineSelect.on( "change", applyFilter );
+
 });
